f8dda49573645349b6528925e19f4b085d07e8c2
saku-naka <saku.goo2006@gmail.com>
2023-08-12 16:32:34 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,22 +99,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Read first is invalid")
       end
       it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
-        @user.last_name = 'Tanaka'
         @user.last_name = 'たなか'
-        @user.last_name = '田中'
-        @user.last_name = '。；；：'
-        @user.last_name = '12345'
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name is invalid")
       end
       it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
-        @user.last_name = 'Tarou'
-        @user.last_name = '12345'
-        @user.last_name = 'たろう'
         @user.last_name = '太郎'
-        @user.last_name = '：；・。、'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
       it '生年月日がないと登録できない' do
         @user.birthday = ''
