f9940fa550cbedf2b8b6ba1c0710de3a0cc59b31
saku-naka <saku.goo2006@gmail.com>
2023-08-15 18:18:54 +0900

    numericalityにonly_integer: trueを追加する

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :postage_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+    validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true}
   end
 
   with_options numericality: { other_than: 0 } do
