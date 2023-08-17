763d516cfd8cc1db0029af73e14d2317e1d8c27c
saku-naka <saku.goo2006@gmail.com>
2023-08-17 11:38:59 +0900

    商品詳細ページのview

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -18,6 +19,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to_root_path
+  end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item,_params)
+    redirect_to_root_path
+  end
+
   def show
   end
 
@@ -25,5 +41,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
